8bf47a907e11aaf132aa68ba5a372c622b29320d
PanPapag <pan_papageorgiou@hotmail.com>
2020-01-14 23:36:11 +0200

    fusion.py completed

--- fusion.py
+++ fusion.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchvision.models.vgg import vgg19
 
 class VGG19(torch.nn.Module):
@@ -16,11 +17,12 @@ class VGG19(torch.nn.Module):
 
     def forward(self, x):
         feature_maps = []
-        for layer in self.features:
+        for idx, layer in enumerate(self.features):
             x = layer(x)
-            if isinstance(layer, nn.modules.ReLU):
+            if idx == 3:
                 feature_maps.append(x)
         return feature_maps
+
 
 class Fusion:
     def __init__(self, input):
@@ -41,25 +43,58 @@ class Fusion:
         A top level method which fuse self.images
         """
         # Convert all images to YCbCr format
-        self.YCBCr_images = []
-        for img in self.input_images:
-            if img.ndim == 3:
-                self.YCBCr_images.append(self._RGB_to_YCbCr(img))
+        self.normalized_images = [-1 for img in self.input_images]
+        self.YCbCr_images = [-1 for img in self.input_images]
+        for idx, img in enumerate(self.input_images):
+            if not self._is_gray(img):
+                self.YCbCr_images[idx] = self._RGB_to_YCbCr(img)
+                self.normalized_images[idx] = self.YCbCr_images[idx][:, :, 0]
+            else:
+                self.normalized_images[idx] = img / 255.
         # Transfer all images to PyTorch tensors
         self._tranfer_to_tensor()
         # Perform fuse strategy
-        self._fuse()
+        fused_img = self._fuse()[:, :, 0]
+        # Reconstruct fused image given rgb input images
+        for idx, img in enumerate(self.input_images):
+            if not self._is_gray(img):
+                self.YCbCr_images[idx][:, :, 0] = fused_img
+                fused_img = self._YCbCr_to_RGB(self.YCbCr_images[idx])
+                fused_img = np.clip(fused_img, 0, 1)
 
+        return (fused_img * 255).astype(np.uint8)
 
     def _fuse(self):
+        """
+        Perform fusion algorithm
+        """
         with torch.no_grad():
-            imgs_to_sum_maps = {}
-            for tensor_img in self.images_to_tensors:
-                imgs_to_sum_maps[tensor_img] = []
+            imgs_sum_maps = [-1 for tensor_img in self.images_to_tensors]
+            for idx, tensor_img in enumerate(self.images_to_tensors):
+                imgs_sum_maps[idx] = []
                 feature_maps = self.model(tensor_img)
                 for feature_map in feature_maps:
                     sum_map = torch.sum(feature_map, dim=1, keepdim=True)
-                    imgs_to_sum_maps[tensor_img].append(sum_map)
+                    imgs_sum_maps[idx].append(sum_map)
+            max_fusion = None
+            for sum_maps in zip(*imgs_sum_maps):
+                features = torch.cat(sum_maps, dim=1)
+                weights = self._softmax(F.interpolate(features,
+                                        size=self.images_to_tensors[0].shape[2:]))
+                weights = F.interpolate(weights,
+                                        size=self.images_to_tensors[0].shape[2:])
+                current_fusion = torch.zeros(self.images_to_tensors[0].shape)
+                for idx, tensor_img in enumerate(self.images_to_tensors):
+                    current_fusion += tensor_img * weights[:,idx]
+                if max_fusion is None:
+                    max_fusion = current_fusion
+                else:
+                    max_fusion = torch.max(max_fusion, current_fusion)
+
+            output = np.squeeze(max_fusion.cpu().numpy())
+            if output.ndim == 3:
+                output = np.transpose(output, (1, 2, 0))
+            return output
 
 
 
@@ -68,7 +103,7 @@ class Fusion:
         A private method which converts an RGB image to YCrCb format
         """
         img_RGB = img_RGB.astype(np.float32) / 255.
-        return cv2.cvtColor(img_RGB, cv2.COLOR_RGB2YCrCb)[:, :, 0]
+        return cv2.cvtColor(img_RGB, cv2.COLOR_RGB2YCrCb)
 
     def _YCbCr_to_RGB(self, img_YCbCr):
         """
@@ -77,12 +112,33 @@ class Fusion:
         img_YCbCr = img_YCbCr.astype(np.float32)
         return cv2.cvtColor(img_YCbCr, cv2.COLOR_YCrCb2RGB)
 
+    def _is_gray(self, img):
+        """
+        A private method which returns True if image is gray, otherwise False
+        """
+        if len(img.shape) < 3:
+            return True
+        if img.shape[2] == 1:
+            return True
+        b, g, r = img[:,:,0], img[:,:,1], img[:,:,2]
+        if (b == g).all() and (b == r).all():
+            return True
+        return False
+
+    def _softmax(self, tensor):
+        """
+        A private method which compute softmax ouput of a given tensor
+        """
+        tensor = torch.exp(tensor)
+        tensor = tensor / tensor.sum(dim=1, keepdim=True)
+        return tensor
+
     def _tranfer_to_tensor(self):
         """
         A private method to transfer all input images to PyTorch tensors
         """
         self.images_to_tensors = []
-        for image in self.input_images:
+        for image in self.normalized_images:
             np_input = image.astype(np.float32)
             if np_input.ndim == 2:
                 np_input = np.repeat(np_input[None, None], 3, axis=1)
@@ -99,3 +155,4 @@ if __name__ == '__main__':
     img2 = cv2.imread('images/MRI-SPECT/tc.png')
     FU = Fusion([img1, img2])
     fusion_img = FU.fuse()
+    cv2.imwrite("img.png", fusion_img)
